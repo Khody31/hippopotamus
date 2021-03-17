b3ac0ca57c967c96e313587e204afcc134796d28
Khody31 <egor.khodasevich@gmail.com>
2021-03-17 09:01:55 +0300

    Painter-fix
    Paint system was fixed

---
+++ player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by egoruskha on 17.03.21 г..
+//
+
+#include "player.h"
---
+++ player.h
@@ -0,0 +1,12 @@
+//
+// Created by egoruskha on 17.03.21 г..
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player {
+
+};
+
+#endif //PLAYER_H
