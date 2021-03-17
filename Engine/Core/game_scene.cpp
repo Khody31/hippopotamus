d3d02fe1be4ee0b53525f83db3a66c4be90dc237
Koshchanka <koschenkovlad@gmail.com>
2021-03-17 09:42:47 +0300

    finally fixed coordinates conversion in repaint

--- Engine/Core/game_scene.cpp
+++ Engine/Core/game_scene.cpp
@@ -1,6 +1,5 @@
 #include <QPainter>
 #include <QKeyEvent>
-#include <iostream>
 #include "game_scene.h"
 #include "Engine/Misc/constants.h"
 #include "game_cotrolls_checker.h"
@@ -28,8 +27,8 @@ void GameScene::paintEvent(QPaintEvent*) {
 }
 
 void GameScene::RenderPixmap(PixmapComponentInterface* pixmap, QPainter& painter) {
-  std::cout << width() << " " << height() << std::endl;
-  std::cout << pixmap->GetSize().x << std::endl;
+  // std::cout << width() << " " << height() << std::endl;
+  // std::cout << pixmap->GetSize().x << std::endl;
   painter.drawPixmap(GetPixmapQRect(pixmap->GetPosition(),
                                     pixmap->GetSize()),
                      pixmap->GetPixmap());
@@ -39,18 +38,16 @@ QRect GameScene::GetPixmapQRect(const Vector2D& pos,
                                 const Vector2D& size) {
   auto map_to_pixel = [this](Vector2D point) {
     point += constants::kMaxCoordinates;
-    point.x *= this->width() / (2 * constants::kMaxCoordinates.x);
+    point.x = point.x * this->width() / (2 * constants::kMaxCoordinates.x);
     point.y *= this->height() / (2 * constants::kMaxCoordinates.y);
     return point;
   };
-  Vector2D upper_left{map_to_pixel(pos) - map_to_pixel(size / 2)};
-  Vector2D lower_right{map_to_pixel(pos) + map_to_pixel(size / 2)};
-  std::cout << upper_left.x << ' ' << upper_left.y << std::endl;
-  std::cout << lower_right.x << " " << lower_right.y << std::endl;
+  Vector2D upper_left{map_to_pixel(pos - size / 2)};
+  Vector2D lower_right{map_to_pixel(pos + size / 2)};
   return QRect(static_cast<int>(upper_left.x),
                static_cast<int>(upper_left.y),
-               static_cast<int>(lower_right.x),
-               static_cast<int>(lower_right.y));
+               static_cast<int>(lower_right.x - upper_left.x),
+               static_cast<int>(lower_right.y - upper_left.y));
 }
 
 void GameScene::keyPressEvent(QKeyEvent* event) {
