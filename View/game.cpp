51142749bea71fa5e3a7174409556491a3cba034
Polina <polchernikova@gmail.com>
2021-03-20 22:43:12 +0300

    updated game widget

--- View/game.cpp
+++ View/game.cpp
@@ -5,32 +5,15 @@ GameWidget::GameWidget(QWidget *parent, AbstractController *controller) :
     QWidget(parent) {
   controller_ = controller;
   game_scene_ = new QWidget(this);
-  CreateButtons();
-  ConnectButtons();
-}
-
-void GameWidget::MenuButtonPressEvent() {
-  controller_->OpenGameMenu();
-}
-
-void GameWidget::PauseButtonPressEvent() {
-controller_->PauseGame();
-}
-
-void GameWidget::CreateButtons() {
-  menu_ = new MenuButton(tr("MENU"), this, QRect(90, 0, 10, 10));
-  pause_ = new MenuButton(tr("PAUSE"), this, QRect(40, 0, 20, 10));
-}
-
-void GameWidget::ConnectButtons() {
-  connect(menu_, &::QPushButton::clicked, this,
-          &::GameWidget::MenuButtonPressEvent);
-  connect(pause_, &::QPushButton::clicked, this,
-          &::GameWidget::PauseButtonPressEvent);
 }
 
 void GameWidget::Resize(QSize size) {
- menu_->setGeometry(menu_->CalculateActualPos(size));
- pause_->setGeometry(pause_->CalculateActualPos(size));
+ game_scene_->resize(size);
+}
+
+void GameWidget::keyPressEvent(QKeyEvent *event) {
+  if(event->key() == Qt::Key_Escape) {
+    controller_->OpenGameMenu();
+  }
 }
 
--- View/game.h
+++ View/game.h
@@ -3,22 +3,19 @@
 
 #include <QWidget>
 #include <QVBoxLayout>
+#include <QKeyEvent>
+
 #include "menu_button.h"
 #include "abstract_controller.h"
 
 class GameWidget: public QWidget {
  public:
   GameWidget(QWidget* parent, AbstractController* controller);
-  void MenuButtonPressEvent();
-  void PauseButtonPressEvent();
   void Resize(QSize size);
+  void keyPressEvent(QKeyEvent* event);
  private:
   QWidget* game_scene_;
-  MenuButton* menu_;
-  MenuButton* pause_;
   AbstractController* controller_;
-  void CreateButtons();
-  void ConnectButtons();
 };
 
 
