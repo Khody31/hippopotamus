7aebcbb06a6a0d9a096855b8f432e555ed014ee3
Polina <polchernikova@gmail.com>
2021-03-20 10:41:24 +0300

    resolve conflixts

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <QApplication>
+#include <QMainWindow>
 
 #include "core/game_scene.h"
 #include "core/connector.h"
 #include "components/components.h"
 #include "systems/transformation_system.h"
+#include "View/button_controller.h"
 
 int main(int argc, char** argv) {
   QApplication app(argc, argv);
-
+  auto* controller = new ButtonController();
   auto connector = std::make_shared<Connector>();
   auto scene = std::make_shared<GameScene>(connector);
-
   return QApplication::exec();
 }
