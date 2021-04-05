7aad02442a9bff33d049f1efd4d6c216780d0ac0
polchernikova <78746457+polchernikova@users.noreply.github.com>
2021-04-05 18:38:18 +0300

    Update game_object.cpp

--- engine/core/game_object.cpp
+++ engine/core/game_object.cpp
@@ -5,6 +5,7 @@
 Component* GameObject::GetComponent(int type_id) {
   return components_[type_id];
 }
+
 void GameObject::AddComponent(Component* comp) {
   if (components_[comp->GetTypeID()]) {
     throw std::logic_error(
@@ -13,11 +14,13 @@ void GameObject::AddComponent(Component* comp) {
   components_[comp->GetTypeID()] = comp;
   comp->SetParent(this);
 }
+
 GameObject::~GameObject() {
   for (auto& comp_ptr : components_) {
     delete comp_ptr.second;
   }
 }
+
 void GameObject::OnPulse(int source_id) {
   const auto& subscribers{GovernorsHandler::Get().GetSubscribers(source_id)};
   for (auto sub : subscribers) {
