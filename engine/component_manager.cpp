b24d5064702210cb392ca432b199cb11443d4eea
polchernikova <78746457+polchernikova@users.noreply.github.com>
2021-04-17 16:47:23 +0300

    Update component_manager.cpp

--- engine/component_manager.cpp
+++ engine/component_manager.cpp
@@ -3,7 +3,7 @@
 ComponentManager::ComponentManager() : next_component_type_(0) {}
 
 void ComponentManager::DestroyEntity(Entity entity) {
-  for (auto const&[type_name, component_array]: component_arrays_) {
+  for (auto const&[type_name, component_array] : component_arrays_) {
     auto const& component = component_array;
 
     component->DestroyEntity(entity);
