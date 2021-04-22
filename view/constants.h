e4043f8746fc65e5169e093c50973907c3e48a37
Polina <polchernikova@gmail.com>
2021-04-22 18:29:25 +0300

    removed codestyle imperfections

--- view/constants.h
+++ view/constants.h
@@ -4,17 +4,17 @@
 
 namespace menu_constants {
 
-  // game menu
+// game menu
 constexpr QRect resume_button = QRect(35, 20, 30, 10);
 constexpr QRect restart_button = QRect(35, 35, 30, 10);
 constexpr QRect sound_button = QRect(35, 50, 30, 10);
 constexpr QRect to_main_menu_button = QRect(35, 65, 30, 10);
-  // main menu
+// main menu
 constexpr QRect start_game_button = QRect(35, 20, 30, 10);
-constexpr QRect settings_button = QRect(35, 35, 30,10);
+constexpr QRect settings_button = QRect(35, 35, 30, 10);
 constexpr QRect exit_button = QRect(35, 50, 30, 10);
-  // settings
+// settings
 constexpr QRect sound = QRect(35, 20, 30, 10);
 constexpr QRect to_main_menu = QRect(35, 35, 30, 10);
 
-}  // menu_constants
+}  // namespace menu_constants
