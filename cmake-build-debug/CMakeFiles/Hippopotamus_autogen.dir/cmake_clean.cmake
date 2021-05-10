c97fd11850c9fa654b0a8ec56cf79330ddde8dc6
Khody31 <egor.khodasevich@gmail.com>
2021-05-10 22:22:08 +0300

    deleted temporary files

--- cmake-build-debug/CMakeFiles/Hippopotamus_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Hippopotamus_autogen"
-  "Hippopotamus_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Hippopotamus_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
