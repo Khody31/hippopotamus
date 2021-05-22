e36e74997ae7c6766e36997e60c4a06297287dc8
Koshchanka <koschenkovlad@gmail.com>
2021-05-22 22:14:55 +0300

    fixed cpplint

--- engine/media_player.cpp
+++ engine/media_player.cpp
@@ -1,15 +1,14 @@
 #include "media_player.h"
 
-using namespace GameSound;
-
-void MediaPlayer::PlaySound(EffectID id_in_enum) {
+void MediaPlayer::PlaySound(GameSound::EffectID id_in_enum) {
   auto id = static_cast<size_t>(id_in_enum);
   sounds_[id].setVolume(volume_);
   sounds_[id].play();
 }
 
 MediaPlayer::MediaPlayer(float volume) : volume_(volume) {
-  sounds_[kEnemyHit].setSource(QUrl::fromLocalFile(":/sound/punch.wav"));
+  sounds_[GameSound::kEnemyHit].setSource(
+      QUrl::fromLocalFile(":/sound/punch.wav"));
 }
 
 void MediaPlayer::SetVolume(float volume) {
--- systems/collision_system.cpp
+++ systems/collision_system.cpp
@@ -4,7 +4,6 @@
 #include "core/connector.h"
 
 #include <unordered_set>
-#include <QSoundEffect>
 
 CollisionSystem::CollisionSystem(Connector* connector,
                                  Coordinator* coordinator,
