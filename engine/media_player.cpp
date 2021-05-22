c5847508c81c3e1a538337ffbc246396741b3cc9
Koshchanka <koschenkovlad@gmail.com>
2021-05-22 22:11:18 +0300

    added sound system

---
+++ engine/media_player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vladislav on 22.05.21.
+//
+
+#include "media_player.h"
---
+++ engine/media_player.h
@@ -0,0 +1,12 @@
+//
+// Created by vladislav on 22.05.21.
+//
+
+#ifndef MEDIA_PLAYER_H
+#define MEDIA_PLAYER_H
+
+class MediaPlayer {
+
+};
+
+#endif //MEDIA_PLAYER_H
